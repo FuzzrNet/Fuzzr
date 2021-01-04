6cf1121c6374dda85b0bc28fdf516892b1ae08b6
kn0wmad <kn0wmad@protonmail.com>
2021-01-04 11:27:46 -0700

    Page Selector Styling

--- src/ui/page_selector.rs
+++ src/ui/page_selector.rs
@@ -8,7 +8,6 @@ pub struct PageButton {
     label_text: String,
     button_state: button::State,
     page_type: PageType,
-    // button_style: ButtonStyle,
 }
 
 #[derive(Debug, Clone)]
@@ -24,13 +23,11 @@ impl PageSelector {
                 label_text: "Publish".to_string(),
                 button_state: button::State::new(),
                 page_type: PageType::Publish,
-                // button_style: ButtonStyle,
             },
             PageButton {
                 label_text: "View".to_string(),
                 button_state: button::State::new(),
                 page_type: PageType::View,
-                // button_style: ButtonStyle,
             },
         ];
 
@@ -40,31 +37,26 @@ impl PageSelector {
                     label_text: "Dashboard".to_string(),
                     button_state: button::State::new(),
                     page_type: PageType::Dashboard,
-                    // button_style: ButtonStyle,
                 },
                 PageButton {
                     label_text: "Feed".to_string(),
                     button_state: button::State::new(),
                     page_type: PageType::Feed,
-                    // button_style: ButtonStyle,
                 },
                 PageButton {
                     label_text: "Site".to_string(),
                     button_state: button::State::new(),
                     page_type: PageType::Site,
-                    // button_style: ButtonStyle,
                 },
                 PageButton {
                     label_text: "Settings".to_string(),
                     button_state: button::State::new(),
                     page_type: PageType::Settings,
-                    // button_style: ButtonStyle,
                 },
                 PageButton {
                     label_text: "Testing".to_string(),
                     button_state: button::State::new(),
                     page_type: PageType::Testing,
-                    // button_style: ButtonStyle,
                 },
             ];
 
@@ -101,27 +93,8 @@ impl PageSelector {
     }
 }
 
-// #[derive(Debug, Clone)]
 mod style {
     use iced::{button, Background, Color};
-
-    const ACTIVE: Color = Color::from_rgb(
-        0xFF as f32 / 255.0,
-        0xFF as f32 / 255.0,
-        0xFF as f32 / 255.0,
-    );
-
-    const HOVERED: Color = Color::from_rgb(
-        0x4B as f32 / 255.0,
-        0x4B as f32 / 255.0,
-        0x4B as f32 / 255.0,
-    );
-
-    const INACTIVE: Color = Color::from_rgb(
-        0x00 as f32 / 255.0,
-        0x00 as f32 / 255.0,
-        0x00 as f32 / 255.0,
-    );
 
     pub enum Button {
         Active { selected: bool },
@@ -133,8 +106,8 @@ mod style {
                 Button::Active { selected } => {
                     if *selected {
                         button::Style {
-                            background: Some(Background::Color(ACTIVE)),
-                            border_radius: 10.0,
+                            background: Some(Background::Color(Color::BLACK)),
+                            border_radius: 3.0,
                             text_color: Color::WHITE,
                             ..button::Style::default()
                         }
@@ -146,21 +119,35 @@ mod style {
         }
 
         fn hovered(&self) -> button::Style {
-            button::Style {
-                background: Some(Background::Color(HOVERED)),
-                border_radius: 3.0,
-                text_color: Color::BLACK,
-                ..self.active()
+            match self {
+                Button::Active { selected } => {
+                    if *selected {
+                        button::Style {
+                            background: Some(Background::Color(Color::BLACK)),
+                            border_radius: 3.0,
+                            text_color: Color::WHITE,
+                            ..button::Style::default()
+                        }
+                    } else {
+                        button::Style {
+                            border_color: Color::BLACK,
+                            border_radius: 3.0,
+                            border_width: 1.0,
+                            text_color: Color::BLACK,
+                            ..button::Style::default()
+                        }
+                    }
+                }
             }
         }
 
-        fn disabled(&self) -> button::Style {
-            button::Style {
-                background: Some(Background::Color(INACTIVE)),
-                border_radius: 3.0,
-                text_color: Color::BLACK,
-                ..self.hovered()
-            }
-        }
+        // fn disabled(&self) -> button::Style {
+        //     button::Style {
+        //         background: Some(Background::Color(INACTIVE)),
+        //         border_radius: 3.0,
+        //         text_color: Color::BLACK,
+        //         ..self.hovered()
+        //     }
+        // }
     }
 }
