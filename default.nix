49237f6a75ec823eb3ee53191260bd944ab2a7b8
Hunter Trujillo <cryptoquick@pm.me>
2021-01-11 09:58:05 -0700

    Nix debugging.

--- default.nix
+++ default.nix
@@ -12,6 +12,9 @@
 }:
 let
   rustOverlay = import "${nixpkgsMozilla}/rust-overlay.nix";
+  rust = (pkgs.latest.rustChannels.nightly.rust.override {
+    targets = [ "x86_64-unknown-linux-musl" ];
+  });
   cargo2nixOverlay = import "${cargo2nix}/overlay";
 
   pkgs = import <nixpkgs> {
@@ -19,8 +22,10 @@ let
     overlays = [ cargo2nixOverlay rustOverlay ];
   };
 
+  # target = "x86_64-unknown-linux-musl";
+
   rustPkgs = pkgs.rustBuilder.makePackageSet' {
-    rustChannel = "stable";
+    rustChannel = "1.49.0";
     packageFun = import ./Cargo.nix;
     packageOverrides =
       let
@@ -33,41 +38,59 @@ let
         servo-freetype-sys = pkgs.rustBuilder.rustLib.makeOverride {
           name = "servo-freetype-sys";
           overrideAttrs = drv: {
-            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.cmake ];
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.cmake pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
           };
         };
         servo-fontconfig-sys = pkgs.rustBuilder.rustLib.makeOverride {
           name = "servo-fontconfig-sys";
           overrideAttrs = drv: {
-            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.file pkgs.cmake pkgs.expat pkgs.freetype ];
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.file pkgs.cmake pkgs.expat pkgs.freetype pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
           };
         };
         x11 = pkgs.rustBuilder.rustLib.makeOverride {
           name = "x11";
           overrideAttrs = drv: {
-            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.x11 ];
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.x11 pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
           };
         };
-        # iced = pkgs.rustBuilder.rustLib.makeOverride {
-        #   name = "iced";
-        #   overrideAttrs = drv: {
-        #     propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.mesa ];
-        #   };
-        # };
-        # wgpu = pkgs.rustBuilder.rustLib.makeOverride {
-        #   name = "wgpu";
-        #   overrideAttrs = drv: {
-        #     propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.mesa ];
-        #   };
-        # };
-        # gfx-backend-vulkan = pkgs.rustBuilder.rustLib.makeOverride {
-        #   name = "gfx-backend-vulkan";
-        #   overrideAttrs = drv: {
-        #     propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.mesa ];
-        #   };
-        # };
+        glow = pkgs.rustBuilder.rustLib.makeOverride {
+          name = "glow";
+          overrideAttrs = drv: {
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
+          };
+        };
+        wgpu = pkgs.rustBuilder.rustLib.makeOverride {
+          name = "wgpu";
+          overrideAttrs = drv: {
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
+          };
+        };
+        iced = pkgs.rustBuilder.rustLib.makeOverride {
+          name = "iced";
+          overrideAttrs = drv: {
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
+          };
+        };
+        iced-wgpu = pkgs.rustBuilder.rustLib.makeOverride {
+          name = "iced-wgpu";
+          overrideAttrs = drv: {
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
+          };
+        };
+        iced-glow = pkgs.rustBuilder.rustLib.makeOverride {
+          name = "iced-glow";
+          overrideAttrs = drv: {
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
+          };
+        };
+        gfx-backend-vulkan = pkgs.rustBuilder.rustLib.makeOverride {
+          name = "gfx-backend-vulkan";
+          overrideAttrs = drv: {
+            propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [ pkgs.vulkan-tools pkgs.vulkan-loader pkgs.vulkan-headers pkgs.mesa ];
+          };
+        };
       in
-      pkgs: pkgs.rustBuilder.overrides.all ++ [ expat-sys servo-freetype-sys servo-fontconfig-sys x11 ];
+      pkgs: pkgs.rustBuilder.overrides.all ++ [ expat-sys servo-freetype-sys servo-fontconfig-sys x11 glow wgpu iced iced-wgpu iced-glow gfx-backend-vulkan ];
   };
 in
 rec {
