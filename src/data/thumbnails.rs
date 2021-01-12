966ea5a71d76778d915511c48c0d6a0d4dd118ff
Hunter Trujillo <cryptoquick@pm.me>
2021-01-12 14:44:51 -0700

    Updated code.

--- src/data/thumbnails.rs
+++ src/data/thumbnails.rs
@@ -85,46 +85,49 @@ where
         let start = self.start.clone();
 
         Box::pin(
-            futures::stream::iter(self.paths).par_then_unordered(None, move |path| {
-                debug!("Processing {:.2?}", &start.elapsed());
+            futures::stream::iter(self.paths).par_map_unordered(None, move |path| {
+                move || {
+                    debug!("Processing {:.2?}", &start.elapsed());
 
-                let image_result = resize_image(&path);
+                    let image_result = resize_image(&path);
 
-                let result = if let Some((decoded_image, (width_px, height_px))) = image_result {
-                    let mut output = Box::new(vec![]);
-                    let mut encoder = JpegEncoder::new(&mut output);
-                    encoder.encode_image(&decoded_image).unwrap();
+                    let result = if let Some((decoded_image, (width_px, height_px))) = image_result
+                    {
+                        let mut output = Box::new(vec![]);
+                        let mut encoder = JpegEncoder::new(&mut output);
+                        encoder.encode_image(&decoded_image).unwrap();
 
-                    let size_bytes = output.len() as u64;
-                    let mime_type = "image/jpeg".to_string();
+                        let size_bytes = output.len() as u64;
+                        let mime_type = "image/jpeg".to_string();
 
-                    let metadata = ImageMetadata {
-                        size_bytes,
-                        mime_type,
-                        width_px,
-                        height_px,
+                        let metadata = ImageMetadata {
+                            size_bytes,
+                            mime_type,
+                            width_px,
+                            height_px,
+                        };
+
+                        let image = output.into_boxed_slice();
+
+                        Progress::Finished(
+                            PathThumb {
+                                path,
+                                image,
+                                metadata,
+                            },
+                            start.elapsed(),
+                        )
+                    } else {
+                        let error = format!(
+                            "Error decoding image after {:.2?}, at: {:?}",
+                            &start.elapsed(),
+                            &path,
+                        );
+                        Progress::Error(error)
                     };
 
-                    let image = output.into_boxed_slice();
-
-                    Progress::Finished(
-                        PathThumb {
-                            path,
-                            image,
-                            metadata,
-                        },
-                        start.elapsed(),
-                    )
-                } else {
-                    let error = format!(
-                        "Error decoding image after {:.2?}, at: {:?}",
-                        &start.elapsed(),
-                        &path,
-                    );
-                    Progress::Error(error)
-                };
-
-                async move { result }
+                    result
+                }
             }),
         )
     }
